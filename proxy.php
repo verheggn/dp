b0beae6604dcfbe5ad0d24212fd08b8223dedae7
Jasper Verheggen <jverheggen@nlad.intra>
2016-04-29 09:52:45 +0200

    setup interface

--- proxy.php
+++ proxy.php
@@ -1,12 +1,42 @@
 <?php
 
+interface iImage {
+  public function getSrc();
+  public function getSize();
+}
+
+class ProxyImage implements iImage {
+
+  private $image;
+
+  public function getSrc () {
+
+  }
+
+  public function getSize () {
+    echo $this -> image;
+  }
+}
+
+class FullImage implements iImage {
+  public function getSrc () {
+
+  }
+
+  public function getSize () {
+
+  }
+}
+
 $full = $_REQUEST['full'];
 $object = (object) [
   "1" => "foo",
   "foo" => "bar"
 ];
+
+$proxyImage = new ProxyImage();
 if ($full !== "undefined") {
   echo json_encode($object);
 } else {
-  echo json_encode((object) null);
+  echo json_encode((object) $proxyImage -> getSize());
 }
